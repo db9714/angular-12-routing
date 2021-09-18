435606eb7939cf08859e9f566a745b88cbff003e
Deepak Bhangale <40859279+db9714@users.noreply.github.com>
2021-09-18 16:50:45 +0530

    Add files via upload

---
+++ src/assets/data/data.ts
@@ -0,0 +1,25 @@
+export const SampleData = [
+    {
+        "id": "1",
+        "name": "SBI bank",
+        "location": "Kalyani Nagar Pune",
+        "status": "open",
+        "logo": "https://media.gettyimages.com/photos/man-uses-a-mobile-phone-outside-the-state-bank-of-india-branch-in-picture-id1199940824?s=612x612"
+    },
+    {
+        "id": "2",
+        "name": "BOI bank",
+        "location": "Nagar-road, Pune",
+        "status": "Closed",
+        "logo": "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2018/04/30/677241-boi-bank-of-india-043018.jpg",
+        "description": "this is bank"
+    },
+    {
+        "id": "3",
+        "name": "HDFC bank",
+        "location": "Magarpatta, Pune",
+        "status": "open",
+        "logo": "https://media.gettyimages.com/photos/man-walks-towards-an-hdfc-bank-ltd-branch-in-mumbai-india-on-saturday-picture-id952877350?s=612x612",
+        "description": "this is bank"
+    }
+]
