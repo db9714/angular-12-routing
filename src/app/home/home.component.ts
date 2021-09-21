49d9c0d4d772945bdcb7757ad2e4ac4c9fe557d5
Deepak Bhangale <40859279+db9714@users.noreply.github.com>
2021-09-21 21:32:35 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SampleData } from "../../assets/data/data"
+import { Router } from '@angular/router';
+import { GetData } from '../shared/services/getData.service';
+import { AppState } from '../shared/services/app-state';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,22 +11,30 @@ import { SampleData } from "../../assets/data/data"
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private getData: GetData,
+    private state: AppState,
+
+  ) { }
   searchKeyword: any = "";
   searchResults: any = [];
   ngOnInit() {
-    if (localStorage.getItem("keyword")?.toString() !== "") {
-      this.searchKeyword = localStorage.getItem("keyword")?.toString();
-      let filteredDate = SampleData.filter(record => record.name.toLowerCase().includes(this.searchKeyword.toLowerCase()))
-      this.searchResults = filteredDate;
-    } else this.searchResults = SampleData
+    this.getData.getSearchData(SampleData).subscribe(
+      (data) => {
+        this.searchResults = data;
+        if (this.state.searchKeyword !== "") {
+          this.searchKeyword = this.state.searchKeyword;
+          this.searchResults = this.searchResults.filter((record: { name: string; }) => record.name.toLowerCase().includes(this.searchKeyword.toLowerCase()))
+        } else this.searchResults = data
+      })
 
   }
   searchData = () => {
     let filteredDate = SampleData.filter(record => record.name.toLowerCase().includes(this.searchKeyword.toLowerCase()))
     this.searchResults = filteredDate;
-    if (this.searchKeyword !== undefined) localStorage.setItem("keyword", this.searchKeyword)
+    if (this.searchKeyword !== undefined) this.state.searchKeyword = this.searchKeyword;
   }
-  navigateToDetails = (itm: any) => window.location.href = "/details/" + itm.id
+  navigateToDetails = (itm: any) => this.router.navigate(["/details/" + itm.id])
 
 }
